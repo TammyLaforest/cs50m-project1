dadcc160e6ec5a3bbdc5dffd22322807c22cc020
TammyLaforest <TammyLaforest@gmail.com>
2019-07-05 09:28:23 -0400

    saving before separating work and break timers

--- App.js
+++ App.js
@@ -5,13 +5,17 @@ import { vibrate } from './utils'
 
 export default class App extends React.Component {
 
+  // Stop: is timer going or paused?
+  // Long: is this the Work (long) timer or the break timer? 
   constructor(props) {
     super(props)
     this.state = {
       stop: true,
       long: true,
-      minutes: 0,
-      seconds: 5
+      minutes: 20,
+      seconds: 0,
+      textMinutes: "",
+      textSeconds: ""
     }
   }
 
@@ -19,43 +23,60 @@ export default class App extends React.Component {
     theInterval = setInterval(this.inc, 1000);
   }
 
-  // Toggles timer button
+  // Stops and starts the timer
   startStopTimer = () => {
+    // Toggle
     this.setState(state => ({
-      stop: !this.state.stop
+      stop: !this.state.stop,
+
     }))
+    // Stop
     if (this.state.stop) {
       clearInterval(theInterval)
     }
+    // Start
     else {
       theInterval = setInterval(this.inc, 1000)
+      this.setState({
+        textMinutes: "",
+        textSeconds: ""
+      })
     }
   }
 
   resetTimer = () => {
+    // Stop the timer
     clearInterval(theInterval)
+
+    // Reset the work timer
     if (this.state.long) {
       this.setState(state => ({
         minutes: 20,
         seconds: 0,
+        textMinutes: "",
+        textSeconds: "",
         stop: true
       }))
     }
+    // Or Reset the break timer
     else {
       this.setState(state => ({
         minutes: 5,
         seconds: 0,
+        textMinutes: "",
+        textSeconds: "",
         stop: true
       }))
     }
+    // Start the timer
     theInterval = setInterval(this.inc, 1000)
-
   }
 
   // Countdown function
   inc = () => {
     // Is timer at 00?
     if (this.state.seconds == 0 && this.state.minutes == 0) {
+      // buzz
       vibrate()
       // Toggle
       this.setState(prevState => ({
@@ -63,10 +84,6 @@ export default class App extends React.Component {
       }));
       // Reset 
       this.resetTimer()
-
-
-
-
     }
     // counts down seconds
     else if (this.state.seconds > 0) {
@@ -83,19 +100,22 @@ export default class App extends React.Component {
     }
   }
 
-
-
   // Toggles long and short timer
   longShortTimer = () => {
+
     this.setState(prevState => ({
-      long: !prevState.long
+      long: !prevState.long,
+      textMinutes: "",
+      textSeconds: ""
     }));
+    // Resets to break timer
     if (this.state.long) {
       this.setState({
         minutes: 5,
         seconds: 0
       })
     }
+    // Resets to work timer
     else {
       this.setState({
         minutes: 20,
@@ -105,21 +125,25 @@ export default class App extends React.Component {
   }
 
   render() {
+
+    // Converts seconds to string of correct length
     let textSeconds = this.state.seconds.toString()
     if (this.state.seconds.toString().length < 2) {
       textSeconds = "0" + this.state.seconds.toString()
     }
+    // Converts minutes to string of correct length
     let textMinutes = this.state.minutes.toString()
     if (this.state.minutes.toString().length < 2) {
       textMinutes = "0" + this.state.minutes.toString()
     }
 
-    // if (this.state.long) {
+
     return (
       <View style={styles.appContainer} >
 
         <Text style={styles.title}>Pomodoro Timer</Text>
 
+        {/* Toggles between work and break label */}
         {this.state.long && <Text style={styles.subtitle}>Work</Text>}
         {!this.state.long && <Text style={styles.subtitle}>Break</Text>}
 
@@ -143,7 +167,6 @@ export default class App extends React.Component {
             raised='true'
           />}
 
-
           {/* always show reset button */}
           <Button
             title="reset"
@@ -152,49 +175,135 @@ export default class App extends React.Component {
             color='gray'
             raised='true'
           />
-
-          {/* show if timer stopped at 0 */}
-          {/* {!this.state.stop && this.state.seconds == 0 && this.state.minutes == 0 && <Button
-          title="reset"
-          onPress={this.resetTimer}
-        />} */}
         </View>
 
-        {/* <View style={styles.inputBox}>
-            <TextInput
-              style={styles.inputContainer}
-              placeholder="MM"
-              name="minutes"
-              keyboardType='numeric'
-              onChangeText={(text) => this.setState({ minutes: text })}
-              maxLength={4}
-            />
-            <TextInput
-              style={styles.inputContainer}
-              placeholder="SS"
-              name="seconds"
-              keyboardType='numeric'
-              onChangeText={(text) => this.setState({ seconds: text })}
-              maxLength={2}
-            />
-          </View> */}
-
+        {/* Displays time remaining */}
         <Text style={styles.numbers}>
           {textMinutes}:{textSeconds}
         </Text>
+
+        {/* Toggles between work and break labels */}
         {this.state.long && <Text >Switch to Break</Text>}
         {!this.state.long && <Text >Switch to Work</Text>}
+
+        {/* Toggles between work and break timers */}
         <Switch
-          onValueChange={this.longShortTimer} value={this.state.long} />
+          onValueChange={this.longShortTimer}
+          value={this.state.long} />
+
+        {/* Change timer length with text input */}
+        <Text>Change Work Timer</Text>
+        <View style={styles.inputBox}>
+
+          <TextInput
+            style={styles.inputContainer}
+            value={this.state.textMinutes}
+            placeholder="MM"
+            name="minutes"
+            keyboardType='numeric'
+            onChangeText={(text) => {
+
+              // stop timer
+              clearInterval(theInterval)
+
+              // reset number
+              this.setState({
+                minutes: text,
+                textMinutes: text,
+                stop: false
+              })
+            }
+            }
+            maxLength={4}
+          />
+          <TextInput
+            style={styles.inputContainer}
+            value={this.state.textSeconds}
+            placeholder="SS"
+            name="seconds"
+            keyboardType='numeric'
+            onChangeText={(text) => {
+
+              // stop timer
+              clearInterval(theInterval)
+
+              // reset number
+              this.setState({
+                seconds: text,
+                textSeconds: text,
+                stop: false
+              })
+            }
+            }
+            maxLength={2}
+          />
+
+        </View>
+        <Text>Change Break Timer</Text>
+        <View style={styles.inputBox}>
+
+          <TextInput
+            style={styles.inputContainer}
+            value={this.state.textMinutes}
+            placeholder="MM"
+            name="minutes"
+            keyboardType='numeric'
+            onChangeText={(text) => {
+
+              // stop timer
+              clearInterval(theInterval)
+
+              // reset number
+              this.setState({
+                minutes: text,
+                textMinutes: text,
+                stop: false
+              })
+            }
+            }
+            maxLength={4}
+          />
+          <TextInput
+            style={styles.inputContainer}
+            value={this.state.textSeconds}
+            placeholder="SS"
+            name="seconds"
+            keyboardType='numeric'
+            onChangeText={(text) => {
+
+              // stop timer
+              clearInterval(theInterval)
+
+              // reset number
+              this.setState({
+                seconds: text,
+                textSeconds: text,
+                stop: false
+              })
+            }
+            }
+            maxLength={2}
+          />
+
+        </View>
       </View>
     )
   }
 }
+// App requirements 
 // - Timer should display minutes and seconds in text   
 // - Timer should count down seconds until it reaches 00:00  
 // - Phone should buzz when timer reaches 0 
 // - Timers should switch between 25 and 5 minutes 
 // - Timer should be able to start, stop, and reset
+
+// ***Actual pomodoro method from Wikipedia ***
+// Decide on the task to be done.
+// Set the pomodoro timer (traditionally to 25 minutes).
+// Work on the task.
+// End work when the timer rings and put a checkmark on a piece of paper.
+// If you have fewer than four checkmarks, take a short break (3–5 minutes), then go to step 2.
+// After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
 
 const styles = StyleSheet.create({
   appContainer: {
